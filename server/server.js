92e03af8aabe5ccaa1533015c22e7c544f004c5d
alejandro.gomez@naranja.com <alejandro.gomez@naranja.com>
2019-06-25 16:13:03 -0300

    rest server

--- server/server.js
+++ server/server.js
@@ -1,42 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('./config/config.js');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario')
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-    if(body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario',
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-});
+mongoose.connect(process.env.URL_DB,
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
+    });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en puerto: ', process.env.PORT);
