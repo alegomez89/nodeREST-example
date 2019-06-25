77ee944af77cf7be09da16263c01d2a8a747c723
alejandro.gomez@naranja.com <alejandro.gomez@naranja.com>
2019-06-25 16:38:41 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://alegomez:hdSdY3ATO9kzepSN@cluster0-quelg.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
